2ecabe54554cbfa5945f2a636e5668117a9778c9
Fatih YILDIZHAN <fatih@momencraft.com>
2015-11-22 16:45:48 +0200

    Initial Commit

---
+++ LimitTextFields/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LimitTextFields
+//
+//  Created by Moment Inc. on 22/11/15.
+//  Copyright © 2015 fatihyildizhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
