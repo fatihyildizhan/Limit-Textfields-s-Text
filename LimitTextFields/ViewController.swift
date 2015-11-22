525f996f432c225e769a4af748a454b2759189fe
Fatih YILDIZHAN <fatih@momencraft.com>
2015-11-22 16:51:57 +0200

    added count and width limit and check for backspace.

--- LimitTextFields/ViewController.swift
+++ LimitTextFields/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// TODO: add keyboard show/hide support
 
+class ViewController: UIViewController {
+  
+  let maxWidth:CGFloat = 200
+  let maxCount:Int = 20
+  
+  @IBOutlet weak var textField: UITextField!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // set the delegate
+    textField.delegate = self
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func sendText(sender: AnyObject) {
+    
   }
-
-
 }
 
+extension ViewController:UITextFieldDelegate {
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    
+    // to allow backspace when limit exceeds
+    if (string.characters.count == 0 && range.length == 1) {
+      return true
+    }
+    
+    // you should check for the specific textfield or these conditions will apply for all of them
+    // don't forget to set delegate.
+    if textField.isEqual(self.textField) {
+      let width:CGFloat = textField.intrinsicContentSize().width
+      let currentText:String = textField.text ?? ""
+      print("width:\(width) - count:\(currentText.characters.count)")
+      
+      if width >= maxWidth {
+        print("width-limit is exceeded")
+        return false
+      }
+      
+      if currentText.characters.count >= maxCount {
+        print("count-limit is exceeded")
+        return false
+      }
+    }
+    return true
+  }
+}
